58d46cfae638826d534c556dcecf93135e98d453
Gol-Stan <loww.101011@gmail.com>
2025-09-28 21:13:08 +0300

    redis + celery install and conf

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Depends
+from sqlalchemy.engine.reflection import cache
 from sqlalchemy.orm import Session
 from .database import SessionLocal, engine, Base
 from . import models, config
+
+from app.celery_worker import add
+from app.redis_client import redis_client
+import time
 
 app = FastAPI()
 
@@ -22,3 +27,20 @@ def db_info():
         "database_url": config.settings.DATABASE_URL,
         "db-user": config.settings.POSTGRES_USER
     }
+
+@app.get("/add")
+def call_add(x:int, y:int):
+    task = add.delay(x, y)
+    return {"task_id": task.id, "status": "started"}
+
+@app.get("/expensive-data/{item_id}")
+def get_expensive_data(item_id: int):
+    cache_key = f"item:{item_id}"
+    cached = redis_client.get(cache_key)
+    if cached:
+        return {"item_id": item_id, "data_cached": cached, "source": "cache"}
+
+    time.sleep(2)
+    data = f"Data for item {item_id}"
+    redis_client.setex(cache_key, 60, data)
+    return {"item_id": item_id, "data_cached": data, "source": "server"}
---
+++ Backend/app/redis_client.py
@@ -0,0 +1,14 @@
+import redis
+import os
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_DB = int(os.getenv("REDIS_DB", 0))
+
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
+
+try:
+    redis_client.ping()
+    print("Redis connected")
+except redis.ConnectionError:
+    print("Connection error")
