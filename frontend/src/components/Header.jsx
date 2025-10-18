45f47cdd84e2813673b5bc91de83771d2189404d
Gol-Stan <loww.101011@gmail.com>
2025-10-18 23:09:31 +0300

    Header logo + background

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,26 @@
+import Nav from './Navbar';
+import logo from '../assets/logo.png';
+import background from '../assets/background.jpg';
+
 export default function Header() {
     return (
-        <header className="bg-white shadow-md py-4 px-6 flex justify-between items-center">
-            <div className="flex items-center space-x-2">
-                <img src="/logo.png" alt="Logo" className="h-10 w-10" />
-                <span className="text-xl font-bold text-blue-800">PetZone</span>
-            </div>
+        <header
+            className="relative bg-cover bg-[position:50%_80%] h-32 flex items-center"
+            style={{
+                backgroundImage: `linear-gradient(rgba(245, 222, 179, 0.2), rgba(245, 222, 179, 0.2)),  url(${background})`,
+            }}
+        >
 
-            <div>
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
-                    Registration / Login
-                </button>
+            <div className="w-full max-w-6xl mx-auto flex justify-between items-center px-6">
+                <div className="flex items-center gap-3">
+                    <img
+                        src={logo}
+                        alt="PetZone Logo"
+                        className="w-16 h-16 object-contain"
+                    />
+                    <h1 className="text-3xl font-bold text-shadow-green-400">PetWorld</h1>
+                </div>
+
             </div>
         </header>
     );
