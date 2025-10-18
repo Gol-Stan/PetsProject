2639aa5420c134b32c8840c295d5b911422f72a7
Gol-Stan <loww.101011@gmail.com>
2025-10-18 23:44:08 +0300

    Header logo + background + footer change

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 text-gray-200 py-4 mt-8">
+        <footer className="bg-orange-100 text-gray-800 py-4 mt-8">
             <div className="max-w-6xl mx-auto px-4 flex justify-between items-center">
                 <div className="w-1/3"></div>
                 <p className="w-1/3 text-center">© 2025 PetsProject</p>
                 <div className="w-1/3 flex justify-end gap-4">
-                    <a href="#" className="hover:text-white">Instagram</a>
-                    <a href="#" className="hover:text-white">Telegram</a>
-                    <a href="#" className="hover:text-white">YouTube</a>
+                    <a href="#" className="hover:text-orange-800">Instagram</a>
+                    <a href="#" className="hover:text-orange-800">Telegram</a>
+                    <a href="#" className="hover:text-orange-800">YouTube</a>
                 </div>
             </div>
         </footer>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
     return (
-        <nav className="bg-blue-800 text-white py-3">
-            <ul className="flex justify-center space-x-8">
+        <nav className="bg-orange-100 text-gray-800 py-3">
+            <ul className="flex justify-center space-x-16">
                 <li><Link to="/breeds" className="hover:underline">Breed</Link></li>
                 <li><Link to="/mypets" className="hover:underline">My Pets</Link></li>
                 <li><Link to="/adoption" className="hover:underline">Adoption</Link></li>
