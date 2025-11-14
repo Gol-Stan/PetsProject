fa6fb880d57e91a237597aee6d03c95ba1c2ac0f
Gol-Stan <loww.101011@gmail.com>
2025-11-15 00:40:59 +0200

    main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,10 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.database import get_db
-from app import models, config
-from app.routers import user, pet, breed
-from app.celery_worker import add
-from app.redis_client import redis_client
-import asyncio
+from app import database
+from app.routers import user, pet, breed, email
+from app.database import engine
 
 app = FastAPI()
 
@@ -28,6 +23,8 @@ app.add_middleware(
 app.include_router(user.router, prefix="/auth")
 app.include_router(pet.router, prefix="/pets")
 app.include_router(breed.router, prefix="/breeds")
+app.include_router(email.router, prefix="/email")
+
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
@@ -36,33 +33,13 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 async def root():
     return {"message": "FastAPI работает! Перейдите на /docs для документации"}
 
-"""@app.get("/")
-async def read_users(db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(models.User))
-    users = result.scalars().all()
-    return users """
+
+@app.on_event("startup")
+async def startup_event():
+    async with engine.begin() as conn:
+        await conn.run_sync(database.Base.metadata.create_all)
 
 
-@app.get("/db-info")
-async def db_info():
-    return {
-        "database_url": config.settings.DATABASE_URL,
-        "db-user": config.settings.POSTGRES_USER
-    }
-
-@app.get("/add")
-async def call_add(x:int, y:int):
-    task = add.delay(x, y)
-    return {"task_id": task.id, "status": "started"}
-
-@app.get("/expensive-data/{item_id}")
-async def get_expensive_data(item_id: int):
-    cache_key = f"item:{item_id}"
-    cached = await redis_client.get(cache_key)
-    if cached:
-        return {"item_id": item_id, "data_cached": cached, "source": "cache"}
-
-    await asyncio.sleep(2)
-    data = f"Data for item {item_id}"
-    await redis_client.setex(cache_key, 60, data)
-    return {"item_id": item_id, "data_cached": data, "source": "server"}
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy", "service": "PetWorld API"}
