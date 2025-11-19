c40bf2f6c06527344bbba74c48d08744f2c437f7
Gol-Stan <loww.101011@gmail.com>
2025-11-19 22:32:40 +0200

    nginx.conf

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,9 +11,12 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost"
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
+        "http://frontend:3000",
+        "http://localhost:80",
     ],
     allow_credentials=True,
     allow_methods=["*"],
